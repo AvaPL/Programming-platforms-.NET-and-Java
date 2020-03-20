abbb7f221a7cf475e6fedc751a375cabe1ae91cf
agngan <agnieszka.gano@gmail.com>
2020-03-20 21:30:23 +0100

    Added two katas.

---
+++ 01_FizzBuzz/FizzBuzz/Kata2_8kyu.cs
@@ -0,0 +1,11 @@
+﻿// Solution to kata: https://www.codewars.com/kata/5769b3802ae6f8e4890009d2/train/csharp
+
+using System.Linq;
+
+public class Kata
+{
+    public static object[] RemoveEveryOther(object[] arr)
+    {
+        return arr.Where((o, i) => i % 2 == 0).ToArray();
+    }
+}
