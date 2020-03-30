dce2f1805b6958dab45e6bfda35f476449b32d5d
Paweł Cembaluk <241574@student.pwr.edu.pl>
2020-03-30 21:27:08 +0200

    Added COVIDStatistics model and view fetched from REST API.

---
+++ DiseaseTracker/DiseaseTracker/DiseaseTracker/Models/COVID19Statistics.cs
@@ -0,0 +1,14 @@
+﻿namespace DiseaseTracker.Models
+{
+    public class COVID19Statistics
+    {
+        public class LatestStatistics
+        {
+            public long Confirmed { get; set; }
+            public long Deaths { get; set; }
+            public long Recovered { get; set; }
+        }
+
+        public LatestStatistics Latest { get; set; }
+    }
+}
