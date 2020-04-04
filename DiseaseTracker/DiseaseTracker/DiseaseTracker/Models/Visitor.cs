8c3613cdbaeacd7095f17fa9841e9d42d014f9f3
Paweł Cembaluk <241574@student.pwr.edu.pl>
2020-04-04 13:44:57 +0200

    Added VisitorsController and related Views.

--- DiseaseTracker/DiseaseTracker/DiseaseTracker/Models/Visitor.cs
+++ DiseaseTracker/DiseaseTracker/DiseaseTracker/Models/Visitor.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Net;
 
 namespace DiseaseTracker.Models
 {
     public class Visitor
     {
         public int Id { get; set; }
-        public IPAddress Ip { get; set; }
+        public string Ip { get; set; }
         public DateTime LastVisit { get; set; }
         public int TotalVisits { get; set; }
     }
