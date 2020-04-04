c6338a76c8e0d6108f82c3eb428443ff7376b2fa
Paweł Cembaluk <241574@student.pwr.edu.pl>
2020-04-04 13:11:51 +0200

    Added Viewer entity and TrackerContext.

---
+++ DiseaseTracker/DiseaseTracker/DiseaseTracker/DAL/TrackerContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using DiseaseTracker.Models;
+
+namespace DiseaseTracker.DAL
+{
+    public class TrackerContext : DbContext
+    {
+        public DbSet<Visitor> Visitors { get; set; }
+    }
+}
---
+++ DiseaseTracker/DiseaseTracker/DiseaseTracker/Models/Visitor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net;
+
+namespace DiseaseTracker.Models
+{
+    public class Visitor
+    {
+        public int Id { get; set; }
+        public IPAddress Ip { get; set; }
+        public DateTime LastVisit { get; set; }
+        public int TotalVisits { get; set; }
+    }
+}
