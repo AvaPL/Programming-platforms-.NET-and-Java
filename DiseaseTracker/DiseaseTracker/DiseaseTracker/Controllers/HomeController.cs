74cf960ada6305386874b05f8cffe5990c00e9da
agngan <ganowicz.agnieszka@gmail.com>
2020-04-27 12:20:07 +0200

    Moved COVID-19 statistics to home page, deleted unused views, and changed application name in layout.

--- DiseaseTracker/DiseaseTracker/DiseaseTracker/Controllers/HomeController.cs
+++ DiseaseTracker/DiseaseTracker/DiseaseTracker/Controllers/HomeController.cs
@@ -16,23 +16,6 @@ namespace DiseaseTracker.Controllers
 
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-            return View();
-        }
-
-        public ActionResult COVID19Statistics()
-        {
             COVID19Statistics statistics = FetchCOVID19Statistics();
             viewModel.Statistics = statistics;
             UpdateVisitor();
